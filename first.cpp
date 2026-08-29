f15ed585e90b6e4c7a50f21cafa1e616bc6b9e45
liyiyangsw <479295761@qq.com>
2026-08-29 18:29:29 +0800

    首次提交：添加 ABC473 题目代码

---
+++ first.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cstdio>
+#include<vector>
+#include<algorithm>
+#include<print>
+typedef long long ll;
+using namespace std;
+int main(){
+    ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+    cout<<__cplusplus<<endl;
+    println("{}",__cplusplus);
+    return 0;
+}
