ad055ab3b2112f180b492773b42a739e14d35131
liyiyangsw <479295761@qq.com>
2026-09-12 19:48:44 +0800

    ABC475_PREPARE

---
+++ ABC475/B.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<cstdio>
+#include<vector>
+#include<algorithm>
+typedef long long ll;
+using namespace std;
+int main(){
+    ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+    return 0;
+}
