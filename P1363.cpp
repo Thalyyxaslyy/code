bd10a2b01569dcbd5597debe4031b0794481c2f9
liyiyangsw <479295761@qq.com>
2026-09-12 19:45:25 +0800

    P1363AC！！

--- P1363.cpp
+++ P1363.cpp
@@ -2,36 +2,42 @@
 #include<cstdio>
 #include<algorithm>
 #include<vector>
-typedef int ll;
+#include<stack>
+#include<tuple>
+typedef long long ll;
 const ll x_[]={0,1,0,-1},y_[]={1,0,-1,0};
 using namespace std;
-ll n,m,sx,sy;;ll vis[1505][1505][3];
-bool map[1505][1505],ans;
-void dfs(ll x,ll y,ll zx,ll zy){
-    if(ans)return;
-    if(vis[x][y][0]&&(vis[x][y][1]!=zx||vis[x][y][2]!=zy)){
-        ans=true;return;
-    }
-    vis[x][y][0]=1;vis[x][y][1]=zx,vis[x][y][2]=zy;
-    for(ll i=0;i<4;i++){
-        ll xx=zx+x_[i],xy=zy+y_[i];
-        ll yx=(xx%n+n)%n,yy=(xy%m+m)%m;
-        if(!map[yx][yy])continue;
-        if(vis[yx][yy][1]!=xx||vis[yx][yy][2]!=yy)
-            dfs(yx,yy,xx,xy);
-    }
-}
+ll n,m,sx,sy;bool map[1501][1501];
+ll vis[1501][1501][3];
+stack<tuple<ll,ll,ll,ll>> wz;
 int main(){
-    ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
-    while(cin>>n>>m){
-        for(ll i=0;i<n;i++)for(ll j=0;j<m;j++){
+	ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+	while(cin>>n>>m){
+		for(ll i=0;i<n;i++)for(ll j=0;j<m;j++){
             char c;cin>>c;
-            if(c=='S')sx=i,sy=j;
-            map[i][j]=(c!='#');
             vis[i][j][0]=0;
-        }ans=false;
-        dfs(sx,sy,sx,sy);
+            vis[i][j][1]=vis[i][j][2]=-1;
+            if(c=='S')sx=i,sy=j;
+            map[i][j]=(c=='#');
+		}
+        while(!wz.empty())wz.pop();
+        wz.push({sx,sy,sx,sy});
+        bool ans=false;
+        while(!wz.empty()){
+            auto[x,y,px,py]=wz.top();wz.pop();
+            if(vis[x][y][0]&&(vis[x][y][1]!=px||vis[x][y][2]!=py)){
+                ans=true;break;
+            }
+            vis[x][y][0]=1,vis[x][y][1]=px,vis[x][y][2]=py;
+            for(ll i=0;i<4;i++){
+                ll xx=px+x_[i],xy=py+y_[i];
+                ll dx=(xx%n+n)%n,dy=(xy%m+m)%m;
+                if(map[dx][dy])continue;
+                if(vis[dx][dy][1]!=xx||vis[dx][dy][2]!=xy||!vis[dx][dy][0])
+                    wz.push({dx,dy,xx,xy});
+            }
+        }
         cout<<(ans?"Yes":"No")<<endl;
-    }
-    return 0;
+	}
+	return 0;
 }
